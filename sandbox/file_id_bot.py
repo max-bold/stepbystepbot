781ce7f7a2c6fea04252cdde502b6006014b23ab
Max Boldyrev <bm456@ya.ru>
2026-01-04 05:24:54 +0300

    Tested reply mode

--- sandbox/file_id_bot.py
+++ sandbox/file_id_bot.py
@@ -23,11 +23,16 @@ bot = Bot(token=bot_key)
 dp = Dispatcher()
 
 upload_mode = False
+
+
 @dp.message(Command("upload"))
 async def upload_command(message: Message):
     global upload_mode
     upload_mode = not upload_mode
-    await message.answer(f"Upload mode is now {'enabled' if upload_mode else 'disabled'}.")
+    await message.answer(
+        f"Upload mode is now {'enabled' if upload_mode else 'disabled'}."
+    )
+
 
 @dp.message()
 async def echo_message(message: Message):
@@ -37,17 +42,19 @@ async def echo_message(message: Message):
     if upload_mode:
         if message.photo:
             for size in message.photo:
-                text = f"Size: {size.width}x{size.height}, {size.file_size/1024/1024:.2f} MB\n\n{size.file_id}"
-                await message.answer(text)
+                text = f"Size: {size.width}x{size.height}, {size.file_size/1024/1024 if size.file_size else 0:.2f} MB\n\n{size.file_id}"
+                await message.reply(text)
         if message.video:
-            await message.answer(message.video.file_id)
+            await message.reply(message.video.file_id)
         if message.video_note:
-            await message.answer(message.video_note.file_id)
+            await message.reply(message.video_note.file_id)
         if message.document:
-            await message.answer(message.document.file_id)
+            await message.reply(message.document.file_id)
         if message.audio:
-            await message.answer(message.audio.file_id)
+            await message.reply(message.audio.file_id)
         if message.voice:
-            await message.answer(message.voice.file_id)
+            await message.reply(message.voice.file_id)
+
+
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot))
